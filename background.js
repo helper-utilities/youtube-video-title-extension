b32d9bfe0ab794ef98e1f6a3b9e3b8cfd8fb82f9
skylerdj <jeffreyjoumjian@hotmail.com>
2020-06-18 19:10:41 +0300

    extension now only works on YouTube video pages

--- background.js
+++ background.js
@@ -1,7 +1,11 @@
 let req;
 
-// if extension icon is clicked => inject copy script
-chrome.browserAction.onClicked.addListener((tab) => {
-	console.log(tab.title)
-	chrome.tabs.executeScript(tab.id, { file: 'copy.js' });
-});
+function runCopyScript() {
+	// if extension icon is clicked => inject copy script
+	chrome.browserAction.onClicked.addListener((tab) => {
+		console.log(tab.title, tab.url)
+		chrome.tabs.executeScript(tab.id, { file: 'copy.js' });
+	});
+}
+
+runCopyScript();
--- copy.js
+++ copy.js
@@ -1,20 +1,22 @@
 // check if window is loaded
-if (document.readyState === 'complete') {
-	let t;
-	let title;
+if (window.location.href.includes('https://www.youtube.com/watch*')) {
+	if (document.readyState === 'complete') {
+		let t;
+		let title;
 
-	// run an interval to make sure the title is loaded to avoid null for querySelectors.
-	t = setInterval(async () => {
-		console.log('interval')
-		title = document.querySelector("#columns #info h1.title").textContent.trim().replace('(lyrics)', '');
+		// run an interval to make sure the title is loaded to avoid null for querySelectors.
+		t = setInterval(async () => {
+			console.log('interval')
+			title = document.querySelector("#columns #info h1.title").textContent.trim().replace('(lyrics)', '');
 
-		if (title) {
-			let permission = await navigator.permissions.query({ name: "clipboard-write" }) === 'granted' || 'prompt' ?
-				window.navigator.clipboard.writeText(title) : undefined;
+			if (title) {
+				let permission = await navigator.permissions.query({ name: "clipboard-write" }) === 'granted' || 'prompt' ?
+					window.navigator.clipboard.writeText(title) : undefined;
 
-			clearInterval(t); // clear interval if title is found
+				clearInterval(t); // clear interval if title is found
 
-		}
+			}
 
-	}, 100)
-};
+		}, 100)
+	};
+}
