9c2f0206528a2aa20328f064088ca9304856db67
skylerdj <jeffreyjoumjian@hotmail.com>
2020-06-21 08:18:44 +0300

    extension now likes the video if not liked already to support the creator

--- copy.js
+++ copy.js
@@ -10,8 +10,15 @@ if (window.location.href.includes('https://www.youtube.com/watch')) {
 			title = document.querySelector("#columns #info h1.title").textContent.trim().replace('(lyrics)', '');
 
 			if (title) {
+				// copy to clipboard if we have permission
 				let permission = await navigator.permissions.query({ name: "clipboard-write" }) === 'granted' || 'prompt' ?
 					window.navigator.clipboard.writeText(title) : undefined;
+
+				// like video if not liked already (support the creator :) )
+				let likeButton = document.querySelector("#top-level-buttons > ytd-toggle-button-renderer.style-scope.ytd-menu-renderer.force-icon-button");
+
+				if (!likeButton.classList.contains('style-default-active'))
+					likeButton.click();
 
 				clearInterval(t); // clear interval if title is found
 
